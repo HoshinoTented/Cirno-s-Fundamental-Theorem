f23f53af5c4d73f9f963b4ef3669c46adb10b89e
HoshinoTented <hoshinotented@qq.com>
2024-03-02 12:10:08 +0800

    sound strategy

--- src/main/java/com/github/hoshinotented/minecraft/cft/config/axioms.kt
+++ src/main/java/com/github/hoshinotented/minecraft/cft/config/axioms.kt
@@ -1,5 +1,6 @@
 package com.github.hoshinotented.minecraft.cft.config
 
+import com.github.hoshinotented.minecraft.cft.event.HookBiteBroadcast
 import net.minecraftforge.common.ForgeConfigSpec
 
 
@@ -12,8 +13,18 @@ class Axioms(builder: ForgeConfigSpec.Builder) {
     val CONFIG_SPEC: ForgeConfigSpec = INSTANCE_AND_CONFIG_SPEC.right
   }
   
-  private val keepUsingCoolDownValue: ForgeConfigSpec.ConfigValue<Int> = builder.comment("The cooldown of keep using (tick), default 0")
+  private val keepUsingCoolDownValue: ForgeConfigSpec.ConfigValue<Int> = builder
+    .comment("The cooldown of keep using (tick), default 0")
     .define("keepUsingCoolDown", 0)
   
+  private val hookBiteStrategyValue: ForgeConfigSpec.EnumValue<HookBiteBroadcast.Strategy> = builder
+    .comment("""
+      Determine how to detect hook biting, default "DataBased", available value:
+      * DataBased: depends on FishingHook.biting, may not work well on server play
+      * SoundBased: depends on sound, may report other player's hook biting
+      """.trimIndent())
+    .defineEnum("hookBiteStrategy", HookBiteBroadcast.Strategy.DataBased)
+  
   val keepUsingCoolDown: Int get() = keepUsingCoolDownValue.get()
+  val hookBiteStrategy: HookBiteBroadcast.Strategy get() = hookBiteStrategyValue.get()
 }
--- src/main/java/com/github/hoshinotented/minecraft/cft/event/fishing.kt
+++ src/main/java/com/github/hoshinotented/minecraft/cft/event/fishing.kt
@@ -1,24 +1,50 @@
 package com.github.hoshinotented.minecraft.cft.event
 
 import com.github.hoshinotented.minecraft.cft.FundamentalTheorem
+import com.github.hoshinotented.minecraft.cft.config.Axioms
 import net.minecraft.client.Minecraft
+import net.minecraft.core.Direction
+import net.minecraft.sounds.SoundEvents
 import net.minecraft.world.entity.projectile.FishingHook
+import net.minecraft.world.phys.Vec3
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.common.MinecraftForge
+import net.minecraftforge.event.PlayLevelSoundEvent
+import net.minecraftforge.event.PlayLevelSoundEvent.AtPosition
 import net.minecraftforge.event.TickEvent
 import net.minecraftforge.event.entity.EntityEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod
+import java.util.EnumSet
 
 data class HookBiteEvent(val hook: FishingHook) : EntityEvent(hook)
 
 @Mod.EventBusSubscriber(value = [ Dist.CLIENT ], modid = FundamentalTheorem.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 object HookBiteBroadcast {
+  
+  /**
+   * The strategy of how to detect hook biting.
+   * * DataBased: Detect hook biting by [FishingHook.biting],
+   *   this is a _more_ sound way, but it may not work well on multi-play.
+   * * SoundBased: Detect hook biting by [PlayLevelSoundEvent.AtPosition],
+   *   this is a _more_ complete way, but it may report other player's hook biting.
+   * * Mix: Detect hook biting by both, unsupported yet.
+   */
+  enum class Strategy {
+    DataBased,
+    SoundBased,
+    // Mix
+  }
+  
   private var lastBiting: Boolean = false
+  private val strategy: Strategy get() = Axioms.INSTANCE.hookBiteStrategy
+  private val AXIS : EnumSet<Direction.Axis> = EnumSet.allOf(Direction.Axis::class.java)
   
   @SubscribeEvent
   @Suppress("unused")
   fun onClientTick(event: TickEvent.ClientTickEvent) {
+    if (strategy != Strategy.DataBased) return
+    
     val mc = Minecraft.getInstance() ?: return
     val player = mc.player ?: return
     val hook = player.fishing ?: return
@@ -29,5 +55,37 @@ object HookBiteBroadcast {
     }
     
     lastBiting = biting
+  }
+  
+  /**
+   * Check whether two sound position is equal
+   * @see net.minecraft.network.protocol.game.ClientboundSoundPacket
+   */
+  private fun checkEqual(left: Vec3, right: Vec3): Boolean {
+    // val planckSize = 8.0
+    val leftPos = listOf(left.x, left.y, left.z).map { it.toInt() }
+    val rightPos = listOf(right.x, right.y, right.z).map { it.toInt() }
+    
+    return leftPos == rightPos
+  }
+  
+  /**
+   * @see net.minecraft.world.entity.Entity.playSound
+   */
+  @SubscribeEvent
+  fun onSoundEvent(event: AtPosition) {
+    if (strategy != Strategy.SoundBased) return
+    if (event.sound?.get() != SoundEvents.FISHING_BOBBER_SPLASH) return
+    if (! event.level.isClientSide) return
+    
+    val mc = Minecraft.getInstance() ?: return
+    val player = mc.player ?: return
+    val hook = player.fishing ?: return
+    val pos = hook.position()
+    val soundPos = event.position
+    
+    if (checkEqual(pos, soundPos)) {
+      MinecraftForge.EVENT_BUS.post(HookBiteEvent(hook))
+    }
   }
 }
